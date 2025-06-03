acf2e9ce4cdbfe45e4a6fd396f40671c6a868f8b
Verda Aslan <153285355+verdasln@users.noreply.github.com>
2025-06-03 18:51:32 +0300

    Update test-algorithm.py

--- test-algorithm.py
+++ test-algorithm.py
@@ -1,1 +1,11 @@
+print(">> Başlıyoruz...")
 
+from utils.point_generator import generate_random_points
+
+points = generate_random_points(5)
+
+print(">> Üretilen Noktalar:")
+for i, p in enumerate(points, 1):
+    print(f"{i}. Nokta: {p}")
+
+input(">> Devam etmek için Enter tuşuna bas...")
